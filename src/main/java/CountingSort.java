b1da9b33c7e86dfbebc1dfa76e584a268d1f6fca
DeborahArauj0 <deborah.araujo@ccc.ufcg.edu.br>
2026-05-15 10:57:51 -0300

    Counting sort finalizado

--- src/main/java/CountingSort.java
+++ src/main/java/CountingSort.java
@@ -34,14 +34,14 @@ public class CountingSort {
     /**
     * Implemente uma versão do counting sort que aceita valor 0 na coleção original.
     */
-    public int[] zeroCountingSort(int[] a, int k) {
-        // TODO implementar
-                //Array auxiliar com tamanho do maior elemento
+    public int[] zeroCountingSort(int[] v, int k) {
+        
+        //Array auxiliar com tamanho do maior elemento
         int[] c = new int[k + 1];
 
         //frequência de cada número no array auxiliar
-        for (int i = 0; i < a.length; i++){
-            c[a[i]] += 1;
+        for (int i = 0; i < v.length; i++){
+            c[v[i]] += 1;
         }
 
         //acumulativa 
@@ -50,26 +50,62 @@ public class CountingSort {
         }
 
         //array final que irá alocar todos os elementos de maneira ordenada
-        int[] b = new int[a.length];
+        int[] b = new int[v.length];
         //ordenação 
-        for (int i = a.length -1; 0 <= i; i--){
-            b[c[a[i]]-1] = a[i]; //adiciona o último elemento do array principal 
-            c[a[i]]--; //decrementa a acumulativa
+        for (int i = v.length -1; 0 <= i; i--){
+            b[c[v[i]]-1] = v[i]; //adiciona o último elemento do array principal 
+            c[v[i]]--; //decrementa a acumulativa
 
         }
 
        return b;
     }
 
-    
-
     /**
     * Implemente uma versão do counting sort que aceita valores negativos na coleção original. Você
     * vai precisar identificar o menor elemento do array. FAça isso no início do método.
     */
     public int[] negativosCountingSort(int[] v, int k) {
-        // TODO implementar
-        return null;
+    
+        int menor = menor(v);
+        
+        int[] c = new int[k - menor + 1];
+
+        //Frequência
+        for (int i = 0; i < v.length; i++){
+            c[v[i]- menor] += 1;
+        }
+
+        //acumulativa
+        for (int i = 1; i < c.length; i++){
+            c[i] += c[i -1];
+        }
+
+        //aloca os elementos do array principal, de forma ordenadeda
+        int[] b = new int[v.length];
+        //ordenação
+        for (int i = v.length -1; i >= 0; i--){
+            b[c[v[i] - menor] -1] = v[i];
+            c[v[i]-menor]--;
+        }
+
+        return b;
+    }
+
+    //método auxiliar para mapear o menor elemento do array
+    public int menor(int[] v){
+
+        int menor = v[0];
+        
+        for (int i = 1; i < v.length; i++){
+            if (v[i] < menor){
+                menor = v[i];
+            }
+
+        }
+
+        return menor;
+
     }
 
 }
--- src/main/java/CountingSortAsserts.java
+++ src/main/java/CountingSortAsserts.java
@@ -18,6 +18,7 @@ public class CountingSortAsserts {
         v = new int[]{1, 1, 1, 2, 2 ,2};
         assert Arrays.equals(sorting.classicCountingSort(v, 2), new int[]{1, 1, 1, 2, 2, 2});
     }
+    
 
     public void zeroCountingSort() {
         CountingSort sorting = new CountingSort();
@@ -36,13 +37,14 @@ public class CountingSortAsserts {
         assert Arrays.equals(sorting.zeroCountingSort(v, 0), new int[]{0, 0, 0});
     }
 
+
     public void negativosCountingSort() {
         CountingSort sorting = new CountingSort();
         int[] v;
 
         v = new int[]{8, 1, 78, 45, -1, -30, -1, 0, 3, 2, 103};
         assert Arrays.equals(sorting.negativosCountingSort(v, 103), new int[]{-30, -1, -1, 0, 1, 2, 3, 8, 45, 78, 103});
-
+;
         v = new int[]{-5};
         assert Arrays.equals(sorting.negativosCountingSort(v, -5), new int[]{-5});
 
@@ -54,5 +56,7 @@ public class CountingSortAsserts {
         new CountingSortAsserts().classicCountingSort();
         new CountingSortAsserts().zeroCountingSort();
         new CountingSortAsserts().negativosCountingSort();
+        new CountingSortAsserts().negativosCountingSort();
+
     }
 }
